0ee6113fc8ad01d7759716c2eb76b4d1eab25efa
Sahwag546 <sahwagkantiwal494@gmail.com>
2024-09-11 15:39:00 +0530

    first commmit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        'fira-sans': ["Fira Sans", "sans-serif"],
+        'exo-2': ["Exo 2", "system-ui"] 
+       },
       backgroundImage: {
         'hero-image': "url('../src/images/bg-img.png')",
       },
